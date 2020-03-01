2894a0991c543162e3b901813813fd2db1b35791
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-01 17:56:26 +0530

    Compare the triplets

--- HackerRankCSharp/SimpleArraySum.cs
+++ HackerRankCSharp/SimpleArraySum.cs
@@ -5,19 +5,19 @@ using System.Linq;
 
 class SimpleArraySum
 {
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int arCount = Convert.ToInt32(Console.ReadLine());
-
-        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
-            ;
-        int result = ar.Sum();
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int arCount = Convert.ToInt32(Console.ReadLine());
+    //
+    //     int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+    //         ;
+    //     int result = ar.Sum();
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
