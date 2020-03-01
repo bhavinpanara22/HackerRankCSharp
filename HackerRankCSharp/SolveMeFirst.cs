e729c5ec26258c512b2d0326010f4b005ace1cfc
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-01 17:49:20 +0530

    Simple Array Sum

--- HackerRankCSharp/SolveMeFirst.cs
+++ HackerRankCSharp/SolveMeFirst.cs
@@ -4,11 +4,11 @@ using System.IO;
 
 class SolveMeFirst
 {
-    static void Main(String[] args)
-    {
-        int val1 = Convert.ToInt32(Console.ReadLine());
-        int val2 = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine(val1 + val2);
-    }
+    // static void Main(String[] args)
+    // {
+    //     int val1 = Convert.ToInt32(Console.ReadLine());
+    //     int val2 = Convert.ToInt32(Console.ReadLine());
+    //
+    //     Console.WriteLine(val1 + val2);
+    // }
 }
