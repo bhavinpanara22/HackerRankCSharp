7d12c9b09349078caee8f829aa2baa80ee2074bf
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-02 16:39:50 +0530

    Staircase

--- HackerRankCSharp/PlusMinus.cs
+++ HackerRankCSharp/PlusMinus.cs
@@ -12,32 +12,27 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class PlusMinusClass
+class PlusMinus
 {
-    // Complete the plusMinus function below.
-    static void PlusMinus(int[] arr)
-    {
-        int pos = 0, neg = 0, zero = 0;
-        foreach (var temp in arr)
-        {
-            if (temp > 0)
-                pos++;
-            else if (temp < 0)
-                neg++;
-            else
-                zero++;
-        }
-
-        Console.WriteLine((float) pos / arr.Length);
-        Console.WriteLine((float) neg / arr.Length);
-        Console.WriteLine((float) zero / arr.Length);
-    }
-
-    static void Main(string[] args)
-    {
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
-        PlusMinus(arr);
-    }
+    // static void Main(string[] args)
+    // {
+    //     int n = Convert.ToInt32(Console.ReadLine());
+    //
+    //     int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+    //     
+    //     int pos = 0, neg = 0, zero = 0;
+    //     foreach (var temp in arr)
+    //     {
+    //         if (temp > 0)
+    //             pos++;
+    //         else if (temp < 0)
+    //             neg++;
+    //         else
+    //             zero++;
+    //     }
+    //
+    //     Console.WriteLine((float) pos / arr.Length);
+    //     Console.WriteLine((float) neg / arr.Length);
+    //     Console.WriteLine((float) zero / arr.Length);
+    // }
 }
