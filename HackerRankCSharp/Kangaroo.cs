72d579acd2d3e74888bcfcd24b3236d71320670c
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-03 14:56:38 +0530

    Between Two Sets

--- HackerRankCSharp/Kangaroo.cs
+++ HackerRankCSharp/Kangaroo.cs
@@ -30,22 +30,22 @@ class Kangaroo
         return willMeet ? "YES" : "NO";
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        string[] x1V1X2V2 = Console.ReadLine().Split(' ');
-
-        int x1 = Convert.ToInt32(x1V1X2V2[0]);
-        int v1 = Convert.ToInt32(x1V1X2V2[1]);
-        int x2 = Convert.ToInt32(x1V1X2V2[2]);
-        int v2 = Convert.ToInt32(x1V1X2V2[3]);
-
-        string result = kangaroo(x1, v1, x2, v2);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     string[] x1V1X2V2 = Console.ReadLine().Split(' ');
+    //
+    //     int x1 = Convert.ToInt32(x1V1X2V2[0]);
+    //     int v1 = Convert.ToInt32(x1V1X2V2[1]);
+    //     int x2 = Convert.ToInt32(x1V1X2V2[2]);
+    //     int v2 = Convert.ToInt32(x1V1X2V2[3]);
+    //
+    //     string result = kangaroo(x1, v1, x2, v2);
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
