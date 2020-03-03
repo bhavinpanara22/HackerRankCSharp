5fea4bfa2d53de9aa9a155a3fd234a9d62c100f9
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-03 16:19:05 +0530

    Divisible Sum Pairs

--- HackerRankCSharp/BIrthdayChocolate.cs
+++ HackerRankCSharp/BIrthdayChocolate.cs
@@ -31,22 +31,22 @@ class BirthdayChocolate
         return toReturn;
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int       n  = Convert.ToInt32(Console.ReadLine().Trim());
-        List<int> s  = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
-        string[]  dm = Console.ReadLine().TrimEnd().Split(' ');
-
-        int d = Convert.ToInt32(dm[0]);
-        int m = Convert.ToInt32(dm[1]);
-
-        int result = Birthday(s, d, m);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int       n  = Convert.ToInt32(Console.ReadLine().Trim());
+    //     List<int> s  = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+    //     string[]  dm = Console.ReadLine().TrimEnd().Split(' ');
+    //
+    //     int d = Convert.ToInt32(dm[0]);
+    //     int m = Convert.ToInt32(dm[1]);
+    //
+    //     int result = Birthday(s, d, m);
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
