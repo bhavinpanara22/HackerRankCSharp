316bcf63b99114b1366389feac03368445da274e
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-03 16:29:14 +0530

    Migratory Birds

--- HackerRankCSharp/DivisibleSumPairs.cs
+++ HackerRankCSharp/DivisibleSumPairs.cs
@@ -14,32 +14,32 @@ using System;
 
 class DivisibleSumPairs
 {
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        string[] nk = Console.ReadLine().Split(' ');
-        int      n  = Convert.ToInt32(nk[0]);
-        int      k  = Convert.ToInt32(nk[1]);
-
-        int[] ar     = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
-        int   result = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (i != n - 1)
-            {
-                for (int j = i + 1; j < n; j++)
-                {
-                    if ((ar[i] + ar[j]) % k == 0)
-                        result++;
-                }
-            }
-        }
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     string[] nk = Console.ReadLine().Split(' ');
+    //     int      n  = Convert.ToInt32(nk[0]);
+    //     int      k  = Convert.ToInt32(nk[1]);
+    //
+    //     int[] ar     = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+    //     int   result = 0;
+    //
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         if (i != n - 1)
+    //         {
+    //             for (int j = i + 1; j < n; j++)
+    //             {
+    //                 if ((ar[i] + ar[j]) % k == 0)
+    //                     result++;
+    //             }
+    //         }
+    //     }
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
---
+++ HackerRankCSharp/MigratoryBirds.cs
@@ -0,0 +1,36 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class MigratoryBirds
+{
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr   = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        List<int> count = new List<int>() {0, 0, 0, 0, 0};
+        foreach (var temp in arr)
+            count[temp - 1]++;
+
+        int maxOccur = count.Max();
+        int result   = count.IndexOf(maxOccur) + 1;
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
