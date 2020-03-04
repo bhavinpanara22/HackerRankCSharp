6c9237a8000bb4f95dd4a20859f725b25b1ab739
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-04 16:40:12 +0530

    Bon Appétit

---
+++ HackerRankCSharp/BonAppetit.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class BonAppetit
+{
+    static void Main(string[] args)
+    {
+        string[] nk = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+        int k = Convert.ToInt32(nk[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int toPay = (bill.Sum() - bill[k]) / 2;
+
+        if (toPay == b)
+            Console.WriteLine("Bon Appetit");
+        else
+            Console.WriteLine(b - toPay);
+    }
+}
