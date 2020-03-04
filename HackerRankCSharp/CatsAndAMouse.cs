9261782b59f2ef2edeeafd5a14ca23eca06466f4
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-04 18:12:37 +0530

    Picking Numbers

--- HackerRankCSharp/CatsAndAMouse.cs
+++ HackerRankCSharp/CatsAndAMouse.cs
@@ -23,26 +23,26 @@ class CatsAndAMouse
         return (aDistance == bDistance) ? "Mouse C" : (aDistance > bDistance) ? "Cat B" : "Cat A";
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++)
-        {
-            string[] xyz = Console.ReadLine().Split(' ');
-
-            int x = Convert.ToInt32(xyz[0]);
-            int y = Convert.ToInt32(xyz[1]);
-            int z = Convert.ToInt32(xyz[2]);
-
-            string result = CatAndMouse(x, y, z);
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int q = Convert.ToInt32(Console.ReadLine());
+    //
+    //     for (int qItr = 0; qItr < q; qItr++)
+    //     {
+    //         string[] xyz = Console.ReadLine().Split(' ');
+    //
+    //         int x = Convert.ToInt32(xyz[0]);
+    //         int y = Convert.ToInt32(xyz[1]);
+    //         int z = Convert.ToInt32(xyz[2]);
+    //
+    //         string result = CatAndMouse(x, y, z);
+    //
+    //         textWriter.WriteLine(result);
+    //     }
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
