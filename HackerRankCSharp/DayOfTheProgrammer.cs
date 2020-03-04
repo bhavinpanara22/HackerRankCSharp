50d4dcb6ffa960e4fc8ee587aab08673cba4adaf
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-04 16:42:28 +0530

    Bon Appétit

--- HackerRankCSharp/DayOfTheProgrammer.cs
+++ HackerRankCSharp/DayOfTheProgrammer.cs
@@ -14,28 +14,28 @@ using System;
 
 class DayOfTheProgrammer
 {
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int year = Convert.ToInt32(Console.ReadLine().Trim());
-
-        DateTime startTime = new DateTime(year: year, month: 1, day: 1);
-        if (year < 1918)
-        {
-            startTime = startTime.AddDays(255);
-
-            if (year % 100 == 0)
-                startTime = startTime.AddDays(-1);
-        }
-        else if (year >= 1918)
-        {
-            startTime = startTime.AddDays((year == 1918) ? 268 : 255);
-        }
-
-        textWriter.WriteLine(startTime.ToString("dd.MM.yyyy"));
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int year = Convert.ToInt32(Console.ReadLine().Trim());
+    //
+    //     DateTime startTime = new DateTime(year: year, month: 1, day: 1);
+    //     if (year < 1918)
+    //     {
+    //         startTime = startTime.AddDays(255);
+    //
+    //         if (year % 100 == 0)
+    //             startTime = startTime.AddDays(-1);
+    //     }
+    //     else if (year >= 1918)
+    //     {
+    //         startTime = startTime.AddDays((year == 1918) ? 268 : 255);
+    //     }
+    //
+    //     textWriter.WriteLine(startTime.ToString("dd.MM.yyyy"));
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
