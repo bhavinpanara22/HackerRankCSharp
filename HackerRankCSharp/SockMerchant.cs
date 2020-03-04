1919866d087ce694757076391cbd28d5996e2dc4
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-04 16:55:32 +0530

    Drawing Book

--- HackerRankCSharp/SockMerchant.cs
+++ HackerRankCSharp/SockMerchant.cs
@@ -14,29 +14,29 @@ using System;
 
 class SockMerchant
 {
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int n = Convert.ToInt32(Console.ReadLine());
-
-        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
-        Array.Sort(ar);
-
-        int result = 0;
-
-        for (int i = 0; i < n -1 ; i++)
-        {
-            if (ar[i] == ar[i + 1])
-            {
-                result++;
-                i++;
-            }
-        }
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int n = Convert.ToInt32(Console.ReadLine());
+    //
+    //     int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+    //     Array.Sort(ar);
+    //
+    //     int result = 0;
+    //
+    //     for (int i = 0; i < n -1 ; i++)
+    //     {
+    //         if (ar[i] == ar[i + 1])
+    //         {
+    //             result++;
+    //             i++;
+    //         }
+    //     }
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
