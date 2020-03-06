beb911ef220238bcc297d5772b9be00a595d101b
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-06 11:34:50 +0530

    Designer PDF Viewer

--- HackerRankCSharp/DesignerPDFViewer.cs
+++ HackerRankCSharp/DesignerPDFViewer.cs
@@ -12,19 +12,27 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class DesignerPDFViewer {
-
+class DesignerPDFViewer
+{
     // Complete the designerPdfViewer function below.
-    static int DesignerPdfViewer(int[] h, string word) {
+    static int DesignerPdfViewer(int[] h, string word)
+    {
+        int width = word.Length, maxHeight = 0;
 
+        foreach (char c in word)
+        {
+            if (h[c - 97] > maxHeight)
+                maxHeight = h[c - 97];
+        }
 
+        return width * maxHeight;
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp))
-            ;
+        int[]  h    = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp));
         string word = Console.ReadLine();
 
         int result = DesignerPdfViewer(h, word);
--- HackerRankCSharp/TheHurdleRace.cs
+++ HackerRankCSharp/TheHurdleRace.cs
@@ -21,21 +21,21 @@ class TheHurdleRace
         return maxHurdleHeight <= k ? 0 : maxHurdleHeight - k;
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-    
-        string[] nk = Console.ReadLine().Split(' ');
-    
-        int n = Convert.ToInt32(nk[0]);
-        int k = Convert.ToInt32(nk[1]);
-    
-        int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp));
-        int   result = HurdleRace(k, height);
-    
-        textWriter.WriteLine(result);
-    
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     string[] nk = Console.ReadLine().Split(' ');
+    //
+    //     int n = Convert.ToInt32(nk[0]);
+    //     int k = Convert.ToInt32(nk[1]);
+    //
+    //     int[] height = Array.ConvertAll(Console.ReadLine().Split(' '), heightTemp => Convert.ToInt32(heightTemp));
+    //     int   result = HurdleRace(k, height);
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
