1ea7ad7ca220b0c5b31d7deb276cc21b6aca598c
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-06 12:51:58 +0530

    Circular Array Rotation

--- HackerRankCSharp/SaveThePrisoner.cs
+++ HackerRankCSharp/SaveThePrisoner.cs
@@ -14,27 +14,27 @@ using System;
 
 class SaveThePrisoner
 {
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int t = Convert.ToInt32(Console.ReadLine());
-
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            string[] nms = Console.ReadLine().Split(' ');
-
-            int n      = Convert.ToInt32(nms[0]);
-            int m      = Convert.ToInt32(nms[1]);
-            int s      = Convert.ToInt32(nms[2]);
-            int result = (s - 1 + m) % n;
-            if (result == 0)
-                result = n;
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int t = Convert.ToInt32(Console.ReadLine());
+    //
+    //     for (int tItr = 0; tItr < t; tItr++)
+    //     {
+    //         string[] nms = Console.ReadLine().Split(' ');
+    //
+    //         int n      = Convert.ToInt32(nms[0]);
+    //         int m      = Convert.ToInt32(nms[1]);
+    //         int s      = Convert.ToInt32(nms[2]);
+    //         int result = (s - 1 + m) % n;
+    //         if (result == 0)
+    //             result = n;
+    //
+    //         textWriter.WriteLine(result);
+    //     }
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
