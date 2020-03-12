e39e462e685daa2b70f143c45a5245eb14863170
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-12 10:52:19 +0530

    Library Fine

--- HackerRankCSharp/SherlockAndSquares.cs
+++ HackerRankCSharp/SherlockAndSquares.cs
@@ -21,24 +21,24 @@ class SherlockAndSquares
         return (int) Math.Floor(Math.Sqrt(b)) - (int) Math.Ceiling(Math.Sqrt(a)) + 1;
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int q = Convert.ToInt32(Console.ReadLine());
-
-        for (int qItr = 0; qItr < q; qItr++)
-        {
-            string[] ab = Console.ReadLine().Split(' ');
-
-            int a      = Convert.ToInt32(ab[0]);
-            int b      = Convert.ToInt32(ab[1]);
-            int result = Squares(a, b);
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     int q = Convert.ToInt32(Console.ReadLine());
+    //
+    //     for (int qItr = 0; qItr < q; qItr++)
+    //     {
+    //         string[] ab = Console.ReadLine().Split(' ');
+    //
+    //         int a      = Convert.ToInt32(ab[0]);
+    //         int b      = Convert.ToInt32(ab[1]);
+    //         int result = Squares(a, b);
+    //
+    //         textWriter.WriteLine(result);
+    //     }
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
