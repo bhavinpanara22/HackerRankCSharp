b5ea1dfbb0dd92cbf4d2bcf07139ab5c7902833e
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-13 16:28:46 +0530

    Lisa's Workbook

---
+++ HackerRankCSharp/LisasWorkbook.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class LisasWorkbook
+{
+    // Complete the workbook function below.
+
+    // Sample Input
+    //
+    // 5 3  
+    // 4 2 6 1 10
+
+    // Sample Output
+    //
+    // 4
+
+    static int Workbook(int n, int k, int[] arr)
+    {
+        int page   = 1;
+        int result = 0;
+
+        foreach (var problem in arr)
+        {
+            for (int i = 1; i <= problem; i++)
+            {
+                if (i == page)
+                    result++;
+
+                if (i % k == 0)
+                    page++;
+            }
+
+            if (problem % k != 0)
+                page++;
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk  = Console.ReadLine().Split(' ');
+        int      n   = Convert.ToInt32(nk[0]);
+        int      k   = Convert.ToInt32(nk[1]);
+        int[]    arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        int result = Workbook(n, k, arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
--- HackerRankCSharp/ServiceLane.cs
+++ HackerRankCSharp/ServiceLane.cs
@@ -29,27 +29,27 @@ class ServiceLane
         return result;
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        string[] nt = Console.ReadLine().Split(' ');
-        int      n  = Convert.ToInt32(nt[0]);
-        int      t  = Convert.ToInt32(nt[1]);
-
-        int[]   width = Array.ConvertAll(Console.ReadLine().Split(' '), widthTemp => Convert.ToInt32(widthTemp));
-        int[][] cases = new int[t][];
-
-        for (int i = 0; i < t; i++)
-        {
-            cases[i] = Array.ConvertAll(Console.ReadLine().Split(' '), casesTemp => Convert.ToInt32(casesTemp));
-        }
-
-        int[] result = ServiceLaneMethod(n, cases, width);
-
-        textWriter.WriteLine(string.Join("\n", result));
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     string[] nt = Console.ReadLine().Split(' ');
+    //     int      n  = Convert.ToInt32(nt[0]);
+    //     int      t  = Convert.ToInt32(nt[1]);
+    //
+    //     int[]   width = Array.ConvertAll(Console.ReadLine().Split(' '), widthTemp => Convert.ToInt32(widthTemp));
+    //     int[][] cases = new int[t][];
+    //
+    //     for (int i = 0; i < t; i++)
+    //     {
+    //         cases[i] = Array.ConvertAll(Console.ReadLine().Split(' '), casesTemp => Convert.ToInt32(casesTemp));
+    //     }
+    //
+    //     int[] result = ServiceLaneMethod(n, cases, width);
+    //
+    //     textWriter.WriteLine(string.Join("\n", result));
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
