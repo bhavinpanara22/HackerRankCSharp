056a7cf24068d5be1665bd51fab69c9b52ea70d6
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-13 19:07:29 +0530

    Fair Rations

--- HackerRankCSharp/FatlandSpaceStations.cs
+++ HackerRankCSharp/FatlandSpaceStations.cs
@@ -47,20 +47,20 @@ class FatlandSpaceStations
         return max;
     }
 
-    static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        string[] nm = Console.ReadLine().Split(' ');
-        int      n  = Convert.ToInt32(nm[0]);
-        int      m  = Convert.ToInt32(nm[1]);
-        int[]    c  = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
-
-        int result = FlatlandSpaceStationsMethod(n, c);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
+    // static void Main(string[] args)
+    // {
+    //     TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+    //
+    //     string[] nm = Console.ReadLine().Split(' ');
+    //     int      n  = Convert.ToInt32(nm[0]);
+    //     int      m  = Convert.ToInt32(nm[1]);
+    //     int[]    c  = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+    //
+    //     int result = FlatlandSpaceStationsMethod(n, c);
+    //
+    //     textWriter.WriteLine(result);
+    //
+    //     textWriter.Flush();
+    //     textWriter.Close();
+    // }
 }
