664aceba13ff17de03ed18f1e47f1c46a80f5a2e
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-20 16:04:46 +0530

    Closest Numbers

---
+++ HackerRankCSharp/ClosestNumbers.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class ClosestNumbers
+{
+	// Complete the closestNumbers function below.
+	static int[] ClosestNumbersMethod(int[] arr)
+	{
+		Array.Sort(arr);
+
+		int minDiff      = int.MaxValue;
+		int minDiffPairs = 0;
+		for (int i = 1; i < arr.Length; i++)
+		{
+			if (arr[i] - arr[i - 1] < minDiff)
+			{
+				minDiff      = arr[i] - arr[i - 1];
+				minDiffPairs = 1;
+			}
+			else if (arr[i] - arr[i - 1] == minDiff)
+			{
+				minDiffPairs++;
+			}
+		}
+
+		int   index  = 0;
+		int[] result = new int[minDiffPairs * 2];
+		for (int i = 0; i < arr.Length - 1; i++)
+		{
+			if (arr[i + 1] - arr[i] == minDiff)
+			{
+				result[index++] = arr[i];
+				result[index++] = arr[i + 1];
+			}
+		}
+
+		return result;
+	}
+
+	static void Main(string[] args)
+	{
+		TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+		int n = Convert.ToInt32(Console.ReadLine());
+
+		int[] arr    = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+		int[] result = ClosestNumbersMethod(arr);
+
+		textWriter.WriteLine(string.Join(" ", result));
+
+		textWriter.Flush();
+		textWriter.Close();
+	}
+}
--- HackerRankCSharp/CountingSort3.cs
+++ HackerRankCSharp/CountingSort3.cs
@@ -4,23 +4,23 @@ using System.IO;
 
 class CountingSort3
 {
-	static void Main(String[] args)
-	{
-		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-
-		int   n      = Convert.ToInt32(Console.ReadLine());
-		int[] counts = new int[100];
-		for (int i = 0; i < n; i++)
-		{
-			int temp = Convert.ToInt32(Console.ReadLine().Split(' ')[0]);
-			counts[temp]++;
-		}
-
-		int sum = 0;
-		for (int i = 0; i < counts.Length; i++)
-		{
-			sum += counts[i];
-			Console.Write(sum + " ");
-		}
-	}
+	// static void Main(String[] args)
+	// {
+	// 	/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+	//
+	// 	int   n      = Convert.ToInt32(Console.ReadLine());
+	// 	int[] counts = new int[100];
+	// 	for (int i = 0; i < n; i++)
+	// 	{
+	// 		int temp = Convert.ToInt32(Console.ReadLine().Split(' ')[0]);
+	// 		counts[temp]++;
+	// 	}
+	//
+	// 	int sum = 0;
+	// 	for (int i = 0; i < counts.Length; i++)
+	// 	{
+	// 		sum += counts[i];
+	// 		Console.Write(sum + " ");
+	// 	}
+	// }
 }
