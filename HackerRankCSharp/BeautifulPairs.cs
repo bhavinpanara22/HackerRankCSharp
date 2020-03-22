9ea27586658a2a3a0a3bf61e81a393e4536df553
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-22 11:14:12 +0530

    Sherlock and The Beast

--- HackerRankCSharp/BeautifulPairs.cs
+++ HackerRankCSharp/BeautifulPairs.cs
@@ -49,19 +49,19 @@ class BeautifulPairs
 		return result;
 	}
 
-	static void Main(string[] args)
-	{
-		TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-		int n = Convert.ToInt32(Console.ReadLine());
-
-		int[] A      = Array.ConvertAll(Console.ReadLine().Split(' '), ATemp => Convert.ToInt32(ATemp));
-		int[] B      = Array.ConvertAll(Console.ReadLine().Split(' '), BTemp => Convert.ToInt32(BTemp));
-		int   result = BeautifulPairsMethod(A, B);
-
-		textWriter.WriteLine(result);
-
-		textWriter.Flush();
-		textWriter.Close();
-	}
+	// static void Main(string[] args)
+	// {
+	// 	TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+	//
+	// 	int n = Convert.ToInt32(Console.ReadLine());
+	//
+	// 	int[] A      = Array.ConvertAll(Console.ReadLine().Split(' '), ATemp => Convert.ToInt32(ATemp));
+	// 	int[] B      = Array.ConvertAll(Console.ReadLine().Split(' '), BTemp => Convert.ToInt32(BTemp));
+	// 	int   result = BeautifulPairsMethod(A, B);
+	//
+	// 	textWriter.WriteLine(result);
+	//
+	// 	textWriter.Flush();
+	// 	textWriter.Close();
+	// }
 }
---
+++ HackerRankCSharp/SherlockAndTheBeast.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class SherlockAndTheBeast
+{
+	// Complete the decentNumber function below.
+	static void DecentNumber(int n)
+	{
+		int c = 5 * ((2 * n) % 3);
+		if (c > n)
+			Console.WriteLine("-1");
+		else
+			Console.WriteLine(new string('5', n - c) + new string('3', c));
+	}
+
+	static void Main(string[] args)
+	{
+		int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+		for (int tItr = 0; tItr < t; tItr++)
+		{
+			int n = Convert.ToInt32(Console.ReadLine().Trim());
+			DecentNumber(n);
+		}
+	}
+}
