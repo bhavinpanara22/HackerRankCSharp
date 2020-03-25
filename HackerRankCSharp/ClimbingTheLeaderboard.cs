8793fd9f25aa0f2fb665d4a0fdea7bb4ce804b65
Bhavinkumar Panara <52033800+bhavinpanara22@users.noreply.github.com>
2020-03-25 11:47:44 +0530

    Extra Long Factorials

--- HackerRankCSharp/ClimbingTheLeaderboard.cs
+++ HackerRankCSharp/ClimbingTheLeaderboard.cs
@@ -43,21 +43,21 @@ class ClimbingTheLeaderboard
 		return result;
 	}
 
-	static void Main(string[] args)
-	{
-		TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-		int scoresCount = Convert.ToInt32(Console.ReadLine());
-
-		int[] scores     = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
-		int   aliceCount = Convert.ToInt32(Console.ReadLine());
-
-		int[] alice  = Array.ConvertAll(Console.ReadLine().Split(' '), aliceTemp => Convert.ToInt32(aliceTemp));
-		int[] result = ClimbingLeaderboard(scores, alice);
-
-		textWriter.WriteLine(string.Join("\n", result));
-
-		textWriter.Flush();
-		textWriter.Close();
-	}
+	// static void Main(string[] args)
+	// {
+	// 	TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+	//
+	// 	int scoresCount = Convert.ToInt32(Console.ReadLine());
+	//
+	// 	int[] scores     = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+	// 	int   aliceCount = Convert.ToInt32(Console.ReadLine());
+	//
+	// 	int[] alice  = Array.ConvertAll(Console.ReadLine().Split(' '), aliceTemp => Convert.ToInt32(aliceTemp));
+	// 	int[] result = ClimbingLeaderboard(scores, alice);
+	//
+	// 	textWriter.WriteLine(string.Join("\n", result));
+	//
+	// 	textWriter.Flush();
+	// 	textWriter.Close();
+	// }
 }
---
+++ HackerRankCSharp/ExtraLongFactorials.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class ExtraLongFactorials
+{
+	// Complete the extraLongFactorials function below.
+	static void ExtraLongFactorialsMethod(int n)
+	{
+		Console.WriteLine(BigFactorial(n));
+	}
+
+	static BigInteger BigFactorial(int n)
+	{
+		if (n <= 1)
+			return 1;
+
+		return n * BigFactorial(n - 1);
+	}
+
+	static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		ExtraLongFactorialsMethod(n);
+	}
+}
